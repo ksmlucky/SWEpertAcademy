232123c88cec5894f5244f83a0f2e628d68c5770
ksmhero <ksmhero@naver.com>
2022-02-22 23:36:25 +0900

    7465 - 창용 마을 무리의 개수

--- src/d4/Num7465_창용마을무리의개수.java
+++ src/d4/Num7465_창용마을무리의개수.java
@@ -61,25 +61,34 @@ public class Num7465_창용마을무리의개수 {
 
     public static void makeSet() {
         parents = new int[N];
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) { // 자기자신의 부모가 자기자신이도록 초기화
             parents[i] = i;
         }
     }
 
     public static int findSet(int a) {
+        // 부모가 자기 자신이면 끝(기저조건?)
         if (a == parents[a]) {
             return a;
         }
+        // 부모가 자기 자신이 아니면 부모로 findSet()을 재귀호출
         return parents[a] = findSet(parents[a]);
     }
 
     public static void union(int a, int b) {
+        // 최상위 부모(루트)를 다른 최상위 부모로 바꾸면(가리키게 하면)
+        // 결과적으로 합집합이되는 것
         int aRoot = findSet(a);
         int bRoot = findSet(b);
         if (aRoot == bRoot) {
             return;
         }
-//        parents[bRoot] = aRoot; // b의 루트가 a의 루트를 가리키게 합친거임
+//        parents[bRoot] = aRoot;
+
+        // 원래는 서로 연결이 있으면 root를 바꾸어 줌으로써 연관관계를 나타내었는데
+        // 예를 들어서 1-3, 2-3, 4-5, 5-6 에서 1-4로 추가해주면 모두 같은 집합이 된다.
+        // 그러므로 모두 1로 바꿔줘야함.
+        // 그러기 위해서 for문을 돌아서 원래 4의 자식이였던 5, 6을 다 1의 자식을 바꿔줌.
         if (aRoot < bRoot) {
             parents[bRoot] = aRoot;
             for (int i = 0; i < N; i++) {

3f2833f016fb8f037ddc6034dec1a42f088fc4c0
ksmhero <ksmhero@naver.com>
2022-04-04 21:48:01 +0900

    1263 - 사람 네트워크2

--- src/d6/Num1263_사람네트워크2.java
+++ src/d6/Num1263_사람네트워크2.java
@@ -17,7 +17,7 @@ public class Num1263_사람네트워크2 {
             for (int i = 0; i < N; ++i) {
                 for (int j = 0; j < N; ++j) {
                     adjMatrix[i][j] = sc.nextInt();
-                    if (i != j && adjMatrix[i][j] == 0) {//자기자신으로의 인접 정보가 아니고 인접해있지 않다면 INF로 채우기
+                    if (i != j && adjMatrix[i][j] == 0) {// 자기자신으로의 인접 정보가 아니고 인접해있지 않다면 INF로 채우기
                         adjMatrix[i][j] = INF;
                     }
                 }
@@ -28,12 +28,12 @@ public class Num1263_사람네트워크2 {
             for (int k = 0; k < N; ++k) {
                 for (int i = 0; i < N; ++i) {
                     if (i == k) {
-                        continue; // 출발지와 경유지가 같다면 다음 출발지}
+                        continue; // 출발지와 경유지가 같다면 다음 출발지
                     }
 
                     for (int j = 0; j < N; ++j) {
                         if (i == j || k == j) {
-                            continue; // 경유지와 목적지가 같거나 출발지가 곧 목적지라면 패스}
+                            continue; // 경유지와 목적지가 같거나 출발지가 곧 목적지라면 패스
                         }
 
                         if (adjMatrix[i][j] > adjMatrix[i][k] + adjMatrix[k][j]) {
